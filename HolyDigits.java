efead83168316dc9544fc880c163ad498f6f9343
ImOedipus <edynowski@luc.edu>
2021-06-16 23:05:36 -0500

    Rethought the method for testing and instantiating values. Finalized

--- HolyDigits.java
+++ HolyDigits.java
@@ -5,39 +5,30 @@ public class HolyDigits {
 
     }
     public static void addressFinder(){
-        int testNumber = 1;
-        int checkCorrect = 0;
-        int a,b,c,d, digitSum;
-        a = 0;
-        b = 0;
-        c = 0;
-        d = 0;
+        int thousands, hundreds, tens, ones;
+        /* wow this was awful and took me forever. So many accidental infinite loops :(
+        anyway this creates a new loop for each digit, and increments it 1-9, then on the innermost loop runs the tests 
+        for the various conditions of the addresS using a series of if statements, one for each condition. 
+        then finall it prints the address once it makes its way through all the if blocks */
+        for (thousands = 0; thousands <= 9; thousands++){
+            for(hundreds = 0; hundreds <= 9; hundreds ++){
+                for (tens = 0; tens <= 9; tens++){
+                    for (ones = 0; ones <= 9; ones++){
+                        if (thousands + hundreds + tens + ones == 27){
+                            if (ones % 2 != 0){
+                                if ((thousands != hundreds) && (hundreds != tens) && (tens != ones) && (hundreds != ones) && (thousands != ones)){
+                                    if (thousands == tens *3){
+                                        System.out.println("The address is " + thousands + hundreds + tens + ones + " Pennsylvania Ave");
 
-        while (checkCorrect == 0){
-            a = testNumber % 10;
-            System.out.println(a);
-            b = testNumber % 100 - a/10;
-            System.out.println(b);
-            c = testNumber % 1000 - (a+b)/100;
-            System.out.println(c);
+                                    }
 
-            d = testNumber % 10000 - (a+b+c)/1000;
-            System.out.println(d);
-
-            digitSum = (a+b+c+d);
-
-            if ((a != b) && (b != c) && (c != d) && (a != c) && (a != d) && (b != d) && (a == (c*3)) && (digitSum == 27) && (d % 2 == 0)){
-                System.out.println("Hello 0");
-                checkCorrect = 1;
-                } else {
-                    System.out.println(checkCorrect);
-                    testNumber++;
-                    System.out.println(testNumber);
-
+                                }
+                            }
+                        }
+                    }
                 }
-                   
+            }
         }
-        System.out.println("The address is: " + a + b + c + d + "Pennsylvania Ave");
-
-    }   
-}
+        
+    }
+}   
