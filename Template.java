dfdbc6c04e30dde3f4be9ff9c24d270180ff68fc
ImOedipus <edynowski@luc.edu>
2021-06-28 11:48:54 -0500

    random stuff idek

--- Template.java
+++ Template.java
@@ -1,34 +1,5 @@
-public class Template {
-	public static void main(String[] args) {
-
-		System.out.println(digitRange(68430));
-		System.out.println(digitRange(131913));
-		System.out.println(digitRange(31));
-		System.out.println(digitRange(5));
-		}
-
-
-	public static int digitRange(int num) {
-
-		int min;
-		min = 10;
-		int max;
-		max = 0;
-		if (num / 10 == 0) {
-			return 1;
-		}
-		while (num != 0) {
-			int digits;
-			digits = num % 10;
-			num = num / 10; // used to get largest number. Once largest number is found, moves on to smallest number
-			if (digits > max){
-				max = digits;
-			}
-			else if (digits < min){ // to get smallest number
-				min = digits;
-			}
-		}
-		return max - min + 1;
-		}
-
+public class Template{
+	public static void main(String[] args){
+		
+	}
 }
